ebffab31576994830b3ce36a4a64e1221887e491
Féres Ben Sassi <feresbensassi5@gmail.com>
2021-06-10 00:33:32 +0100

    add chart

--- blog-frontend/src/Root.js
+++ blog-frontend/src/Root.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import App from 'components/App';
-import { Provider } from 'react-redux';
-import configure from 'store/configure';
-import { Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import App from "components/App";
+import { Provider } from "react-redux";
+import configure from "store/configure";
+import { Route } from "react-router-dom";
 
 const store = configure();
 
@@ -15,6 +15,6 @@ const Root = () => {
       </BrowserRouter>
     </Provider>
   );
-}
+};
 
 export default Root;
--- blog-frontend/src/pages/PostListPage.js
+++ blog-frontend/src/pages/PostListPage.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import PageTemplate from 'components/common/PageTemplate'
-import PostListContainer from 'containers/PostListContainer'
-import HeaderContainer from 'containers/HeaderContainer'
-
+import React from "react";
+import PageTemplate from "components/common/PageTemplate";
+import PostListContainer from "containers/PostListContainer";
+import HeaderContainer from "containers/HeaderContainer";
+import Chart from "../components/Chart";
 const PostListPage = () => {
   return (
-    <PageTemplate header={<HeaderContainer/>}>
-      <PostListContainer/>
+    <PageTemplate header={<HeaderContainer />}>
+      <PostListContainer />
+      <Chart />
     </PageTemplate>
   );
 };
